1d425db0669973a8d29cb412c654d47b9e4f6a16
baohoang031111 <tan.thach36@gmail.com>
2023-06-08 00:08:12 +0700

    simple slender c1

--- excercise/abc/CountDownTimer/index.js
+++ excercise/abc/CountDownTimer/index.js
@@ -1,20 +1,44 @@
-function timekhuyenmai(tgkm){
+function timekhuyenmai(tgkm) {
     const z1 = setInterval(() => {
 
-        let now =  Date.now();
+        let now = Date.now();
 
         let someMillisecondValue = (tgkm - now) / 1000;
         //! tính giay //
         let ngay = someMillisecondValue / (24 * 3600);
+
         kq = `${Math.trunc(ngay)} ngay `;
-        let gio =  someMillisecondValue % (24 * 3600) / 3600;
+        let gio = someMillisecondValue % (24 * 3600) / 3600;
         kq += `${Math.trunc(gio)} gio `;
-        let phut =  someMillisecondValue % (24 * 3600) % 3600 / 60;
+        let phut = someMillisecondValue % (24 * 3600) % 3600 / 60;
         kq += `${Math.trunc(phut)} phut `;
-        let giay =  someMillisecondValue % (24 * 3600) % 3600 % 60;
+        let giay = someMillisecondValue % (24 * 3600) % 3600 % 60;
         kq += `${Math.trunc(giay)} giay `;
         console.log(kq);
-    },1000)
+    }, 1000)
 }
+window.addEventListener('load', handlefunction);
+function handlefunction() {
+    const days = document.querySelector(".days");
+    const hours = document.querySelector(".hours");
+    const minutes = document.querySelector(".minutes");
+    const seconds = document.querySelector(".seconds");
+    let tgkm = new Date(2023, 5, 7, 0, 0, 0);
+    const z1 = setInterval(() => {
 
-timekhuyenmai(new Date(2023,5,7,0,0,0));
+        let now = Date.now();
+
+        let someMillisecondValue = (tgkm - now) / 1000;
+        //! tính giay //
+        let ngay = someMillisecondValue / (24 * 3600);
+        days.innerHTML = Math.trunc(ngay);
+        let gio = someMillisecondValue % (24 * 3600) / 3600;
+        hours.innerHTML = Math.trunc(gio);
+        gio.innerHTML = Math.trunc(gio);
+        let phut = someMillisecondValue % (24 * 3600) % 3600 / 60;
+        minutes.innerHTML = Math.trunc(phut);
+
+        let giay = someMillisecondValue % (24 * 3600) % 3600 % 60;
+        seconds.innerHTML = Math.trunc(giay);
+    }, 1000)
+}
